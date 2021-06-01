d98b63dd764d43a31a5a285ac4aa957fdef73960
Tyler Jones <tylerjones321@gmail.com>
2021-06-01 15:30:31 -0700

    Finished Configuration of Environment Variables

---
+++ Configuration/Configuration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCommandLine(args);
+
+
+            IConfigurationRoot config = builder.Build();
+
+            IConfigurationSection section = config.GetSection("Config");
+
+            IEnumerable<IConfigurationSection> group = section.GetChildren();
+
+            foreach (IConfigurationSection item in group)
+            {
+                Console.WriteLine(item.Value.ToString());
+            }
+
+            Console.WriteLine(config.GetSection("Config:WindowSize").Value.ToString());
+
+            // The precedence of the environment variables is the same as the order
+            // in which they are added to the configuration builder
+        }
+    }
+
+}
